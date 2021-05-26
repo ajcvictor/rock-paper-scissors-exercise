1c1b18d387751d00b63c786aa902b89467945e71
ajcvictor <ajv269@stern.nyu.edu>
2021-05-25 20:43:02 -0400

    Validated user inputs

--- game.py
+++ game.py
@@ -2,8 +2,27 @@
 
 print("Rock, Paper, Scissors, Shoot!")
 
+# first define the variable of user_choice
+
 user_choice = input("Please choose one of 'rock' , 'paper' , 'scissors': ")
+
+# then print the variable
+
 
 print("USER CHOICE:" , user_choice)
 
+#validate the input such that it is only one of the expected values
+#... will we continue with the rest of the program
+#...otherwise we'll stop the program before it tries to do anything else
+#...and we'll ask the user to run the program again
 
+
+#if user_choice = "rock" or "paper" or "scissors:"
+if (user_choice == "rock") or (user_choice == "paper") or (user_choice == "scissors" ):
+    print ("VALID. KEEP GOING")
+else:
+    print("OOPS, invalid input. Please try again.")
+    exit()
+
+
+print("THIS IS THE END OF OUR GAME. PLEASE PLAY AGAIN.")
